a17813481625ee28742e3b00447cde09b879790b
elizabethwidiasri <elizabethwidiasri@gmail.com>
2020-02-02 21:24:39 +0700

    search and promo bar

--- gojek/App.js
+++ gojek/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={{flex: 1, marginTop: StatusBar.currentHeight }}>
-      <View style={{flex: 1, backgroundColor: 'pink'}}>
-        <Text>clone Gojek App</Text>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <View style={{marginHorizontal: 17, flexDirection: 'row'}}>
+          <View style={{position: 'relative', flex: 1}}>
+            <TextInput placeholder="What do you want to eat?" style={{marginTop: 7, borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18}} />
+            <Image source={require('./assets/icon/search.png')} style={{position: 'absolute', top: 15, left: 12}} />
+          </View>
+          <View style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+            <Image source={require('./assets/icon/promo.png')} />
+          </View>
+        </View>
       </View>
       <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
