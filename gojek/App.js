faaa9744140bc7b178032d6a30ce9037ae1b87d0
elizabethwidiasri <elizabethwidiasri@gmail.com>
2020-02-03 15:12:36 +0700

    voucher section

--- gojek/App.js
+++ gojek/App.js
@@ -112,12 +112,35 @@ export default function App() {
           </View>
           <View style={{paddingTop: 16, paddingBottom: 20, borderBottomColor: '#E8E9ED', borderBottomWidth: 1}}>
             <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', letterSpacing: 1 }}>GO-NEWS</Text>
-            <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A', marginBottom: 11 }}>Dimas drajat selamatkan penalti, Timnas U-23 kalahkan Brunei</Text>
+            <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A', marginBottom: 11 }}>Dimas Drajad selamatkan penalti, Timnas U-23 kalahkan Brunei</Text>
             <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'flex-end', borderRadius: 4}}>
               <Text style={{fontWeight: 'bold', fontSize: 13, color: 'white'}}>READ</Text>
             </TouchableOpacity>
 
           </View>
+        </View>
+        {/* gofood banner  */}
+        <View style={{paddingTop: 16, paddingHorizontal: 16, marginBottom: 16, paddingBottom: 16, borderBottomColor: '#E8E9ED', borderBottomWidth: 1}} >
+          <View style={{position: 'relative'}}>
+          <Image source={require('./assets/dummy/food-banner.jpg')} style={{height: 170, width: '100%', borderRadius: 6}} />
+          <View style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, backgroundColor: 'black', opacity: 0.15, borderRadius: 6}}></View>
+          <View style={{height: 15, width: 55, position: 'absolute', top: 16, left: 16}}>
+            <Image source={require('./assets/logo/white.png')} style={{width: undefined, height: undefined, resizeMode: 'contain', flex: 1}} />
+          </View>
+          <View style={{position: 'absolute', bottom: 0, left: 0, width: '100%', flexDirection: 'row', alignItems: 'center', marginTop: 10, paddingHorizontal: 16, paddingBottom: 16}}>
+            <View>
+              <Text style={{fontSize: 22, fontWeight: 'bold', color: 'white', marginBottom: 2, marginRight: 15}}>Free GO-FOOD voucher</Text>
+              <Text style={{fontSize: 17, color: 'white', fontWeight: '400'}}>Quick, before they run out!</Text>
+            </View>
+            <View style={{flex: 1, paddingLeft: 50}}>
+              <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'stretch', borderRadius: 4}}>
+                <Text style={{fontWeight: 'bold', fontSize: 13, color: 'white'}}>GET VOUCHER</Text>
+              </TouchableOpacity>
+
+            </View>
+          </View>
+          </View>
+
         </View>
         </ScrollView>
       </View>
